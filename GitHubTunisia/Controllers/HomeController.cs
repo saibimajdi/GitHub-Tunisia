433459eacd2bc4dd4cae6d21bd9fc440f6c5ce93
majdi saibi <saibimajdi@outlook.com>
2017-04-21 16:45:14 +0200

    add home page

--- GitHubTunisia/Controllers/HomeController.cs
+++ GitHubTunisia/Controllers/HomeController.cs
@@ -3,28 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Octokit;
+using System.Configuration;
+using System.Threading.Tasks;
 
 namespace GitHubTunisia.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var client = new GitHubClient(new ProductHeaderValue("github-tunisia"));
+            client.Credentials = new Credentials(ConfigurationManager.AppSettings["GitHubToken"]);
+
+            var searchUserRequest = new SearchUsersRequest("location:tunisia")
+            {
+                Location = "Tunisia",
+                SortField = UsersSearchSort.Followers
+            };
+
+            var searchResult = await client.Search.SearchUsers(searchUserRequest);
+
+            List<Octokit.User> users = new List<Octokit.User>();
+
+            foreach (var item in searchResult.Items)
+                users.Add(await client.User.Get(item.Login));
+
+            return View(users.Take(10).ToList());
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> Repos()
         {
-            ViewBag.Message = "Your application description page.";
+            var client = new GitHubClient(new ProductHeaderValue("github-tunisia"));
+            client.Credentials = new Credentials(ConfigurationManager.AppSettings["GitHubToken"]);
 
-            return View();
+            var searchReposRequest = new SearchRepositoriesRequest("location:tunisia")
+            {
+                SortField = RepoSearchSort.Stars,
+            };
+
+            var searchResult = await client.Search.SearchRepo(searchReposRequest);
+
+            List<Octokit.Repository> repos = new List<Repository>();
+
+            foreach (var repo in searchResult.Items)
+                repos.Add(await client.Repository.Get(repo.Id));
+
+            return View(repos);
         }
 
-        public ActionResult Contact()
+        public ActionResult Coders()
         {
-            ViewBag.Message = "Your contact page.";
+            var client = new GitHubClient(new ProductHeaderValue("github-tunisia"));
+            client.Credentials = new Credentials(ConfigurationManager.AppSettings["GitHubToken"]);
 
-            return View();
+            var searchUserRequest = new SearchUsersRequest("location:tunisia")
+            {
+                Location = "Tunisia",
+                SortField = UsersSearchSort.Followers
+            };
+
+            var searchResult = client.Search.SearchUsers(searchUserRequest).Result;
+
+            List<Octokit.User> users = new List<Octokit.User>();
+
+            foreach (var item in searchResult.Items)
+                users.Add(client.User.Get(item.Login).Result);
+
+            return View(users);
         }
     }
 }
